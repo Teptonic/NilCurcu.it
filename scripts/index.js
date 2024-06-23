ede2ca424e3931b417cdd567ca4cadf2b229a6b7
SOF_blur <78835401+KaiRocks2006@users.noreply.github.com>
2024-06-22 17:20:18 -0700

    added load funcs

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
 window.addEventListener('click', function() {
     
 
+    // Get the element to fade out
+    const element = document.getElementById('waitForClick');
+
+    element.style.opacity = 0;
+
+
     setTimeout(function(){
         var audio = document.getElementById('background-music');
-        audio.volume = 0.5;  // Set volume to 50%
+        audio.volume = 0.1;  // Set volume to 50%
         audio.play();
-    }, 500)
+    }, 250)
 });
