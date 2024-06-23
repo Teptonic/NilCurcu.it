607800204201593c7310720b252a6e5f4c3536eb
SOF_blur <78835401+KaiRocks2006@users.noreply.github.com>
2024-06-22 21:25:33 -0700

    Added random songs

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,52 @@
 const loader = document.getElementById('waitForClick');
 const music = document.getElementById('background-music');
 
-window.addEventListener('click', function() {
-    
 
+function loadSite(){
     loader.style.opacity = 0;
+
+    let randomSong = Math.floor(Math.random() * 5)
+    console.log(randomSong)
+
+
+    // Chooses random song
+
+
+    if (randomSong == 0){
+        music.src = './sounds/dodge_this.mp3'
+    } else if (randomSong == 1) {
+        music.src = './sounds/speed_is_life.mp3'
+    } else if (randomSong == 2) {
+        music.src = './sounds/CuteDepressed.mp3'
+    } else if (randomSong == 3) {
+        music.src = './sounds/wyw.mp3'
+    } else if (randomSong == 4) {
+        music.src = './sounds/Young_Kid.mp3'
+    }
+
 
 
     setTimeout(function(){
-        music.volume = 0.1;  // Set volume to 50%
+        music.volume = 0.075;  // Set volume to 50%
         music.play();
         loader.remove()
     }, 350)
-});
-
-
-function toggleMusic(){
-    if (toggled == true){
-        music.pause();
-        document.getElementById('toggleMusic').src = '../images/pause.png';
-        toggled = !toggled;
-    } else if (toggled == false){
-        music.play();
-        document.getElementById('toggleMusic').src = '../images/play.png';
-        toggled = !toggled;
-    }
+    window.removeEventListener('click', loadSite)
 }
 
-document.getElementById('toggleMusic').addEventListener('click', toggleMusic)
+window.addEventListener('click', loadSite);
+
+
+
+const audio = document.getElementById('backgroundAudio');
+const volumeControl = document.getElementById('volumeControl');
+
+volumeControl.addEventListener('input', function() {
+    audio.volume = this.value;
+});
+
+// Set initial volume
+audio.volume = volumeControl.value;
+
+// Set initial volume
+music.volume = volumeControl.value;
